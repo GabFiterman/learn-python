def6048317edf06adff614276ab93bbfeed42670
Fiterman <gfiterman96@gmail.com>
2023-08-22 12:09:59 -0300

    intial commit: just a hello world

---
+++ helloWorld.py
@@ -0,0 +1,5 @@
+message='hello world!'
+print(message)
+
+secondMessage='Lembre-se de criar um ambiente virtual, no terminal integrado: python -m venv learn-python && ./learn-python/Scripts/activate'
+print(secondMessage)
